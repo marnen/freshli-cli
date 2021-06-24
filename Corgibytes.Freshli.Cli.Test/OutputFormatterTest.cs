886e2ac78895990b69e557d31388bd366200c1b7
Chris C <chris.cumming@saturdaymp.com>
2021-06-24 15:36:48 -0600

    Fixed a date localization bug in the unit tests.

--- Corgibytes.Freshli.Cli.Test/OutputFormatterTest.cs
+++ Corgibytes.Freshli.Cli.Test/OutputFormatterTest.cs
@@ -9,27 +9,26 @@ namespace Corgibytes.Freshli.Cli.Test
 {
     public class OutputFormatterTest
     {
-        private static IList<(DateTime Date, double Value, bool UpgradeAvailable, bool Skipped)> DatesAndValues
+        private static IEnumerable<(DateTime Date, double Value, bool UpgradeAvailable, bool Skipped)> DatesAndValues
         {
             get
             {
-                return new List<(
-        DateTime Date, double Value, bool UpgradeAvailable, bool Skipped)>
-      {
-        (new DateTime(2010, 01, 01), 1.1010, false, false),
-        (new DateTime(2010, 02, 01), 2.2020, false, false),
-        (new DateTime(2010, 03, 01), 3.3030, false, false),
-        (new DateTime(2010, 04, 01), 4.4040, false, false),
-        (new DateTime(2010, 05, 01), 5.5050, false, false),
-        (new DateTime(2010, 06, 01), 6.6060, false, false),
-        (new DateTime(2010, 07, 01), 7.7070, false, false),
-        (new DateTime(2010, 08, 01), 8.8080, false, false),
-        (new DateTime(2010, 09, 01), 9.9090, false, false),
-        (new DateTime(2010, 10, 01), 10.0101, false, false),
-        (new DateTime(2010, 11, 01), 11.1111, true, false),
-        (new DateTime(2010, 12, 01), 12.2121, true, false),
-        (new DateTime(2011, 01, 01), 13.3333, false, true)
-      };
+                return new List<(DateTime Date, double Value, bool UpgradeAvailable, bool Skipped)>
+                {
+                    (new DateTime(2010, 01, 01), 1.1010, false, false),
+                    (new DateTime(2010, 02, 01), 2.2020, false, false),
+                    (new DateTime(2010, 03, 01), 3.3030, false, false),
+                    (new DateTime(2010, 04, 01), 4.4040, false, false),
+                    (new DateTime(2010, 05, 01), 5.5050, false, false),
+                    (new DateTime(2010, 06, 01), 6.6060, false, false),
+                    (new DateTime(2010, 07, 01), 7.7070, false, false),
+                    (new DateTime(2010, 08, 01), 8.8080, false, false),
+                    (new DateTime(2010, 09, 01), 9.9090, false, false),
+                    (new DateTime(2010, 10, 01), 10.0101, false, false),
+                    (new DateTime(2010, 11, 01), 11.1111, true, false),
+                    (new DateTime(2010, 12, 01), 12.2121, true, false),
+                    (new DateTime(2011, 01, 01), 13.3333, false, true)
+                };
             }
         }
 
@@ -38,8 +37,9 @@ namespace Corgibytes.Freshli.Cli.Test
             IList<MetricsResult> results = new List<MetricsResult>();
             foreach (var dateAndValue in DatesAndValues)
             {
-                var result = new LibYearResult();
-                result.Add(new LibYearPackageResult
+                var result = new LibYearResult
+                {
+                    new LibYearPackageResult
                     (
                         "test_package",
                         "1.0",
@@ -50,7 +50,7 @@ namespace Corgibytes.Freshli.Cli.Test
                         dateAndValue.UpgradeAvailable,
                         dateAndValue.Skipped
                     )
-                );
+                };
                 results.Add(new MetricsResult(dateAndValue.Date, "N/A", result));
             }
 
@@ -60,24 +60,27 @@ namespace Corgibytes.Freshli.Cli.Test
         private static string EnglishHeader = "Date\tLibYear\tUpgradesAvailable\tSkipped";
         private static string SpanishHeader = "Fecha\tAñoLib\tActualizaciónesDisponibles\tOmitida";
 
+        private static string BoolToString(bool b)
+        {
+            return Convert.ToInt32(b).ToString();
+        }
+
         private static string ExpectedDatesAndValues(string header)
         {
-            StringWriter expected = new StringWriter();
-                expected.WriteLine(header);
-                expected.WriteLine("2010/01/01\t1.1010\t0\t0");
-                expected.WriteLine("2010/02/01\t2.2020\t0\t0");
-                expected.WriteLine("2010/03/01\t3.3030\t0\t0");
-                expected.WriteLine("2010/04/01\t4.4040\t0\t0");
-                expected.WriteLine("2010/05/01\t5.5050\t0\t0");
-                expected.WriteLine("2010/06/01\t6.6060\t0\t0");
-                expected.WriteLine("2010/07/01\t7.7070\t0\t0");
-                expected.WriteLine("2010/08/01\t8.8080\t0\t0");
-                expected.WriteLine("2010/09/01\t9.9090\t0\t0");
-                expected.WriteLine("2010/10/01\t10.0101\t0\t0");
-                expected.WriteLine("2010/11/01\t11.1111\t1\t0");
-                expected.WriteLine("2010/12/01\t12.2121\t1\t0");
-                expected.WriteLine("2011/01/01\t0.0000\t0\t1");
-                return expected.ToString();
+            var expected = new StringWriter();
+            expected.WriteLine(header);
+
+            foreach (var dv in DatesAndValues)
+            {
+                string date = dv.Date.ToString(CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern);
+                double value = dv.Skipped ? 0 : dv.Value;  // Skipped results should result in a zero value displayed.
+                string upgrade = BoolToString(dv.UpgradeAvailable);
+                string skipped = BoolToString(dv.Skipped);
+
+                expected.WriteLine($"{date}\t{value:F4}\t{upgrade}\t{skipped}");
+            }
+
+            return expected.ToString();
         }
 
         [Fact]
